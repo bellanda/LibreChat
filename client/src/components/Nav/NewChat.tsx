63d916cf2979e9eecbb1453e536ee67dc7bd9f10
bellanda <gustavocasadeibellanda@gmail.com>
2025-05-27 09:04:21 -0300

    🎨 UI changes  - add possibility to translate model names to custom names  - add New Chat button to sidebar

--- client/src/components/Nav/NewChat.tsx
+++ client/src/components/Nav/NewChat.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback } from 'react';
-import { useRecoilValue } from 'recoil';
-import { useNavigate } from 'react-router-dom';
 import { useQueryClient } from '@tanstack/react-query';
-import { QueryKeys, Constants } from 'librechat-data-provider';
-import type { TMessage, TStartupConfig } from 'librechat-data-provider';
-import { NewChatIcon, MobileSidebar, Sidebar } from '~/components/svg';
-import { getDefaultModelSpec, getModelSpecPreset } from '~/utils';
-import { TooltipAnchor, Button } from '~/components/ui';
+import type { TMessage } from 'librechat-data-provider';
+import { Constants, QueryKeys } from 'librechat-data-provider';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MobileSidebar, NewChatIcon, Sidebar } from '~/components/svg';
+import { Button, TooltipAnchor } from '~/components/ui';
 import { useLocalize, useNewConvo } from '~/hooks';
 import store from '~/store';
 
@@ -70,7 +68,8 @@ export default function NewChat({
           }
         />
         <div className="flex">
-          {headerButtons}
+          {/* {headerButtons} */}
+
           <TooltipAnchor
             description={localize('com_ui_new_chat')}
             render={
@@ -79,9 +78,12 @@ export default function NewChat({
                 variant="outline"
                 data-testid="nav-new-chat-button"
                 aria-label={localize('com_ui_new_chat')}
-                className="rounded-full border-none bg-transparent p-2 hover:bg-surface-hover md:rounded-xl"
+                className="w-full rounded-full border-none bg-transparent p-2 hover:bg-surface-hover md:rounded-xl"
                 onClick={clickHandler}
               >
+                <p className="flex items-center justify-center text-sm text-text-primary">
+                  + Novo Chat
+                </p>
                 <NewChatIcon className="icon-md md:h-6 md:w-6" />
               </Button>
             }
---
+++ client/src/utils/translateModelsNames.ts
@@ -0,0 +1,25 @@
+const modelsDisplayNames = {
+  '': '',
+  // QWEN MODELS
+  'qwen3-235b-a22b': 'qwen3-high (235b)',
+  'qwen3-32b': 'qwen3-medium (32b)',
+  'qwen3-4b': 'qwen3-mini (4b)',
+  // OPENAI MODELS
+  'chatgpt-4o-latest': 'gpt-4o',
+  'o3-2025-04-16': 'o3',
+  'o4-mini-2025-04-16': 'o4-mini',
+  'gpt-4.1-2025-04-14': 'gpt-4.1',
+  'gpt-4.1-mini-2025-04-14': 'gpt-4.1-mini',
+  'gpt-4.1-nano-2025-04-14': 'gpt-4.1-nano',
+  // ANTHROPIC MODELS
+  'claude-sonnet-4-20250514': 'claude-4-sonnet',
+  'claude-opus-4-20250514': 'claude-4-opus',
+  // Google MODELS
+  //   GOOGLE_MODELS=gemini-2.5-pro-preview-05-06,gemini-2.5-flash-preview-04-17,gemini-2.0-flash-001,gemini-2.0-flash-lite-001
+  'gemini-2.5-pro-preview-05-06': 'gemini-2.5-pro',
+  'gemini-2.5-flash-preview-04-17': 'gemini-2.5-flash-preview',
+  'gemini-2.0-flash-001': 'gemini-2.0-flash',
+  'gemini-2.0-flash-lite': 'gemini-2.0-flash-lite',
+};
+
+export default modelsDisplayNames;
