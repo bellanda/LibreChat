5205c1802c12162f7fbab145f62230edd991a014
bellanda <gustavocasadeibellanda@gmail.com>
2025-05-27 12:44:43 -0300

    🎨 add hpe logo

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -1,15 +1,15 @@
+import { getConfigDefaults, Permissions, PermissionTypes } from 'librechat-data-provider';
 import { useMemo } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { getConfigDefaults, PermissionTypes, Permissions } from 'librechat-data-provider';
 import type { ContextType } from '~/common';
-import ModelSelector from './Menus/Endpoints/ModelSelector';
-import { PresetsMenu, HeaderNewChat, OpenSidebar } from './Menus';
 import { useGetStartupConfig } from '~/data-provider';
-import ExportAndShareMenu from './ExportAndShareMenu';
-import { useMediaQuery, useHasAccess } from '~/hooks';
-import BookmarkMenu from './Menus/BookmarkMenu';
-import { TemporaryChat } from './TemporaryChat';
+import { useHasAccess, useMediaQuery } from '~/hooks';
 import AddMultiConvo from './AddMultiConvo';
+import ExportAndShareMenu from './ExportAndShareMenu';
+import { HeaderNewChat, OpenSidebar, PresetsMenu } from './Menus';
+import BookmarkMenu from './Menus/BookmarkMenu';
+import ModelSelector from './Menus/Endpoints/ModelSelector';
+import { TemporaryChat } from './TemporaryChat';
 
 const defaultInterface = getConfigDefaults().interface;
 
@@ -40,9 +40,16 @@ export default function Header() {
           {!navVisible && <OpenSidebar setNavVisible={setNavVisible} />}
           {!navVisible && <HeaderNewChat />}
           {<ModelSelector startupConfig={startupConfig} />}
+
           {interfaceConfig.presets === true && interfaceConfig.modelSelect && <PresetsMenu />}
           {hasAccessToBookmarks === true && <BookmarkMenu />}
           {hasAccessToMultiConvo === true && <AddMultiConvo />}
+          <img
+            className="ml-[10px] h-[37px] w-full rounded-[10px] bg-white object-contain"
+            // src="/assets/logohpe-preto.svg"
+            src="/assets/hpe.png"
+            alt=""
+          />
           {isSmallScreen && (
             <>
               <ExportAndShareMenu
