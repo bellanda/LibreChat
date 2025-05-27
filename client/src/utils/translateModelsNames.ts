8ab2a2510ea356d277a7d54fd8e3a11a84700804
bellanda <gustavocasadeibellanda@gmail.com>
2025-05-27 12:44:43 -0300

    🎨 add hpe logo

--- client/src/utils/translateModelsNames.ts
+++ client/src/utils/translateModelsNames.ts
@@ -18,7 +18,7 @@ const modelsDisplayNames = {
   'gemini-2.5-pro-preview-05-06': 'gemini-2.5-pro',
   'gemini-2.5-flash-preview-04-17': 'gemini-2.5-flash-preview',
   'gemini-2.0-flash-001': 'gemini-2.0-flash',
-  'gemini-2.0-flash-lite': 'gemini-2.0-flash-lite',
+  'gemini-2.0-flash-lite-001': 'gemini-2.0-flash-lite',
   // Alibaba Models
   'qwen3-235b-a22b': 'qwen3-high (235b)',
   'qwen3-32b': 'qwen3-medium (32b)',
