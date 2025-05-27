036860bdf66bdfe840bb3ef7eb5f11463f39b600
bellanda <gustavocasadeibellanda@gmail.com>
2025-05-27 09:21:29 -0300

    ✨ add groq models to translateModelsNames.ts

--- client/src/utils/translateModelsNames.ts
+++ client/src/utils/translateModelsNames.ts
@@ -1,25 +1,33 @@
 const modelsDisplayNames = {
+  // When no model is selected, display an empty string
   '': '',
-  // QWEN MODELS
-  'qwen3-235b-a22b': 'qwen3-high (235b)',
-  'qwen3-32b': 'qwen3-medium (32b)',
-  'qwen3-4b': 'qwen3-mini (4b)',
-  // OPENAI MODELS
+  // OpenAI Models
+  // OPENAI_MODELS=chatgpt-4o-latest,o3-2025-04-16,o4-mini-2025-04-16,gpt-4.1-2025-04-14,gpt-4.1-mini-2025-04-14,gpt-4.1-nano-2025-04-14
   'chatgpt-4o-latest': 'gpt-4o',
   'o3-2025-04-16': 'o3',
   'o4-mini-2025-04-16': 'o4-mini',
   'gpt-4.1-2025-04-14': 'gpt-4.1',
   'gpt-4.1-mini-2025-04-14': 'gpt-4.1-mini',
   'gpt-4.1-nano-2025-04-14': 'gpt-4.1-nano',
-  // ANTHROPIC MODELS
+  // Anthropic Models
+  // ANTHROPIC_MODELS=claude-sonnet-4-20250514,claude-opus-4-20250514
   'claude-sonnet-4-20250514': 'claude-4-sonnet',
   'claude-opus-4-20250514': 'claude-4-opus',
-  // Google MODELS
-  //   GOOGLE_MODELS=gemini-2.5-pro-preview-05-06,gemini-2.5-flash-preview-04-17,gemini-2.0-flash-001,gemini-2.0-flash-lite-001
+  // Google Models
+  // GOOGLE_MODELS=gemini-2.5-pro-preview-05-06,gemini-2.5-flash-preview-04-17,gemini-2.0-flash-001,gemini-2.0-flash-lite-001
   'gemini-2.5-pro-preview-05-06': 'gemini-2.5-pro',
   'gemini-2.5-flash-preview-04-17': 'gemini-2.5-flash-preview',
   'gemini-2.0-flash-001': 'gemini-2.0-flash',
   'gemini-2.0-flash-lite': 'gemini-2.0-flash-lite',
+  // Alibaba Models
+  'qwen3-235b-a22b': 'qwen3-high (235b)',
+  'qwen3-32b': 'qwen3-medium (32b)',
+  'qwen3-4b': 'qwen3-mini (4b)',
+  // Groq Models
+  'meta-llama/llama-4-maverick-17b-128e-instruct': 'llama-4-maverick (High 17b-128e)',
+  'meta-llama/llama-4-scout-17b-16e-instruct': 'llama-4-scout (Medium 17b-16e)',
+  'meta-llama/llama-guard-4-12b': 'llama-4-guard (Mini 12b)',
+  'llama-3.3-70b-versatile': 'llama-3.3-70b-versatile (Balanced 70b)',
 };
 
 export default modelsDisplayNames;
